5086bda2242077205b4886719fbbbb2a5b256598
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-16 12:52:48 -0800

    built out Deck class

--- PlayCards/Card.cs
+++ PlayCards/Card.cs
@@ -1,54 +1,55 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PlayCards
 {
-        public enum Suit { Hearts, Spades, Clubs, Diamonds };
-        public enum Rank
-        {
-            Ace = 1,
-            Two = 2,
-            Three = 3,
-            Four = 4,
-            Five = 5,
-            Six = 6,
-            Seven = 7,
-            Eight = 8,
-            Nine = 9,
-            Ten = 10,
-            Jack = 11,
-            Queen = 12,
-            King = 13
-        };
-        enum Color { Red, Black } // derived from Suit
+    public enum CardSuit { Hearts, Spades, Clubs, Diamonds };
+    public enum CardRank
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    };
+    enum CardColor { Red, Black } // derived from Suit
 
-    class Card
+    public class Card
     {
 
         // set backing stores
-        private Suit _suit;
-        private Rank _rank;
-        private Color _color; // derived from SuitOfCard
+        private CardSuit _suit;
+        private CardRank _rank;
+        private CardColor _color; // derived from SuitOfCard
         // redirect target for attempts to change static values
-        private Suit _garbageSuit;
-        private Rank _garbageRank;
-        private Color _garbageColor;
+        private CardSuit _garbageSuit;
+        private CardRank _garbageRank;
+        private CardColor _garbageColor;
 
         // card properties (suit, rank, color)
-        public Suit SuitOfCard
+        public CardSuit Suit
         {
             get { return _suit; }
             set { _garbageSuit = value; }
         }
 
-        public Rank RankOfCard
+        public CardRank Rank
         {
             get { return _rank; }
             set { _garbageRank = value; }
         }
 
-        public Color ColorOfCard // derived from SuitOfCard
+        public CardColor Color // derived from SuitOfCard
         {
             get { return _color; }
             set { _garbageColor = value; }
@@ -59,18 +60,19 @@ namespace PlayCards
         /// </summary>
         /// <param name="suit"> hearts, spades, clubs, diamonds </param>
         /// <param name="rank"> face value of card (Ace, Two, Jack,...) </param>
-        public Card(Suit suit, Rank rank)
+        public Card(CardSuit suit, CardRank rank)
         {
             _suit = suit;
             _rank = rank;
-            if ( _suit == Suit.Hearts || _suit == Suit.Diamonds )
+            if ( _suit == CardSuit.Hearts || _suit == CardSuit.Diamonds )
             {
-                _color = Color.Red;
+                _color = CardColor.Red;
             }
             else
             {
-                _color = Color.Black;
+                _color = CardColor.Black;
             }
         }
+
     }
 }
--- PlayCards/Deck.cs
+++ PlayCards/Deck.cs
@@ -1,10 +1,72 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PlayCards
 {
-    class Deck
+    class Deck<T> : IEnumerable<T>
     {
+        // establish collector array, initially 5 card slots
+        T[] deckContents = new T[5];
+        // establish index variable for traversing and manipulating collector array
+        int current = 0;
+
+        //public object Current => throw new NotImplementedException();
+
+        public void Add(T cardToAdd)
+        {
+            if (current > deckContents.Length-2) // extend array BEFORE it's completely full
+            {
+                Array.Resize(ref deckContents, deckContents.Length + 5); // resize deck by 5 slots at a time
+            }
+            deckContents[current] = cardToAdd;
+            current++;
+        }
+
+        public void Remove(int cardToRemove)
+        {
+            for (int i = cardToRemove; i < current; i++)
+            {
+                if ( cardToRemove.Equals(deckContents[i]))
+                {
+                    deckContents[i] = deckContents[current-1];
+                }
+            }
+            current--;
+        }
+
+        public int Count(Deck<T> deckToCount)
+        {
+            return current + 1;
+        }
+
+
+        // IEnumerator implementation:
+
+        /// <summary>
+        /// Steps through the DeckContents array and returns its elements one by one.
+        /// (Literally: It enumerates the DeckContents array so that the elements can be used as an iterator.)
+        /// </summary>
+        /// <returns> contents of DeckContents array, element by element </returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < current; i++)
+            {
+                yield return deckContents[i];
+            }
+        }
+
+        /// <summary>
+        /// When enumeration called without data type indicator, runs the GetEnumerator method using custom data type indicator.
+        /// </summary>
+        /// <returns> contents of DeckContents array, element by element </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+
     }
 }
